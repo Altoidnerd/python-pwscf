e9f651ad9d7d62b8e5a2ae92320c00185163d1ef
Allen Majewski <almno10@dev1.ufhpc>
2016-05-24 21:16:34 -0400

    namelist dictionary style working

--- test/namelist.old-list.py
+++ test/namelist.old-list.py
@@ -42,43 +42,29 @@ class Namelist(object):
     valid_namelists = uppercase + [ item.lower() for item in uppercase ]
     
     self.name = name
-    
     if content is None:
-        self.content = dict()
+        self.content = []
     else:
-        if type(content) != dict:
-          raise TypeError("Namelist(name=\"\", content=None)\n\tIf content is not None, content must be of type dict")
+        if type(content) != list:
+          raise TypeError("Namelist(name=\"\", content=None)\n\tIf content is not None, content must be of type list")
         self.content = content
 
     if self.name not in valid_namelists:
       raise ValueError('{} is not a valid namelist.'.format(self.name))
-
-
-  def get_value(self, var):
-    return self.content[var]
-  
-  def set_value(self, var, val):
-    self.content[var] = val
+        
     
+  def __str__(self):
+    return '\t&{}\n{}{}'.format(self.name, li_to_str(self.content),'/\n')
+
+  def __repr__(self):
+    return self.__str__()
+ 
+  def set_value(self, var, val):
+    self.content.append(format_pair(var, val))
+
   def set_multiple(self, var_list, val):
     for var in var_list:
       self.set_value(var, val)
-  
-  def __str__(self):
-    s = ""
-    s += '\t{}{}\n'.format('&', self.name)
-    for k in self.content.keys():
-      s += "{}='{}'\n".format(k, self.content[k])
-    s += '/\n'
-    return s   
-
-  def __repr__(self):
-     return self.__str__()
-
-
-#  def set_value(self, var, val):
- #   self.content.append(format_pair(var, va;))
-
   
   def get_index(self, var):
     return self.content.index(var)
