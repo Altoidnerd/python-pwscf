55e56db9b367f506c4631633cc68101198b2b9b8
= <=>
2016-05-27 00:36:42 -0400

    wew lad

--- example.py
+++ example.py
@@ -3,39 +3,49 @@
 from namelist import Namelist
 
 control = Namelist(name="CONTROL")
-control.set_value("calculation",'scf')
-control.set_value("prefix", "scf.pbe.ecut-80")
-control.set_value("outdir", "./scratch")
-control.set_value("pseudo_dir", "/home/altoidnerd/PSEUDOPOTENTIALS/")
-control.set_value("verbosity","high")
-control.set_value("title","p-dichlorobenzene-efg")
-control.set_value("restart_mode","from_scratch")
-control.set_value("forc_conv_thr",1.0e-4)
-control.set_value("disk_io","low")
-control.set_value("wfcollect",".false.")
-control.set_value("tstress", ".true.")
-control.set_multiple(["lorbm", "lberry", "ikpoint_dir"], ".true.")
+control.set_val("calculation",'scf')
+control.set_val("prefix", "scf.pbe.ecut-80")
+control.set_val("outdir", "./scratch")
+control.set_val("pseudo_dir", "/home/altoidnerd/PSEUDOPOTENTIALS/")
+control.set_val("verbosity","high")
+control.set_val("title","p-dichlorobenzene-efg")
+control.set_val("restart_mode","from_scratch")
+control.set_val("forc_conv_thr",1.0e-4)
+control.set_val("disk_io","low")
+control.set_val("wfcollect",".false.")
 
+for key in ["tstress", "tprnfor","lorbm", "lberry", "ikpoint_dir"]:
+  control.set_val(key, ".true.")
 
 system = Namelist("SYSTEM")
-system.set_value("ibrav", 0)
-system.set_value('a', 9.558)
-system.set_value('c', "8.981")
-system.set_value("ecutwfc", 60)
-system.set_value("ecutrho", 600)
-system.set_value("occupations", 'smearing')
-system.set_value('smearing', 'fermi-dirac')
-system.set_value('degauss', .00367)
-system.set_value('nat', 70)
-system.set_value('ntyp', '6')
+key_value_pairs = [
+	("ibrav", 0),
+	('a', 9.558),
+	('c', "8.981"),
+	("ecutwfc", 60),
+	("ecutrho", 600),
+	("occupations", 'smearing'),
+	('smearing', 'fermi-dirac'),
+	('degauss', .00367),
+	('nat', 70),
+	('ntyp', '6') ]
 
+for (key, value) in key_value_pairs:
+    system.set_val(key, value)
+
+electrons_dict={ 
+	"conv_thr": 		1e-7,
+	"diago_thr_init": 	1e-4,
+	"mixing_beta":		0.7,
+	"diagonalization":	'david'
+		}
 
 electrons = Namelist("ELECTRONS")
-electrons.set_value("conv_thr", 1e-7)
-electrons.set_value("diago_thr_init", 1e-4)
-electrons.set_value("mixing_beta", '0.7')
-electrons.set_value('diagonalization', 'david')
 
+for key in electrons_dict.keys():
+  value = electrons_dict[key]
+  electrons.set_val(key, value)
+ 
 print(control, system, electrons)
 
 
