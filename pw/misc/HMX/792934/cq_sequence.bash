62c71fad4e290d26d0b5d7b72fdf4a2984e75c57
Allen Majewski <almno10@dev2.ufhpc>
2016-09-06 01:46:43 -0400

    fixes

--- pw/misc/HMX/792934/cq_sequence.bash
+++
@@ -1,2 +0,0 @@
-
-for k in $(cat row-equivalent | cut -b -3); do ngrep Cq * | ngrep -E "N    ?$k " && echo -e ; done
--- pw/misc/HMX/script.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-
-test_list = ['Hello, world!', 14, 'heyyyy']
-
