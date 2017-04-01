41168c0e53f338346116e80a5a4f42c9fb251429
altoidnerd <allenmajs1@gmail.com>
2017-04-01 07:10:20 -0400

    updates

--- 10.single_molecule/structures/rotate.5.py
+++ 10.single_molecule/structures/rotate.5.py
@@ -3,25 +3,21 @@ import matrix as m
 import numpy as np
 import sys
 
-# We attempt to roate the molcule to correct orientation
-# by roating the molecule about the z axis
  
-x = md.Md('tt.pwi')
+x = md.Md('t.pwi')
 
 positions = x.get_trajectory2()[0]
 
 
 
 
-# now we need to rotate about the x-axis
+# now we need to rotate about the z-axis
 # by pi/2
-# obtain vector connecting C3, C5
-# calcuclate the angle between cvec and the x-axis
 pos=positions
 theta=np.pi/2
 
 t=theta
 for i in pos:
-  print('C    '+str( m.rotx(t) @ i).strip('[]').replace('  ', '     ').replace(' -','    '))
+  print('C    '+str( m.rotz(t) @ i).strip('[]').replace('  ', '     ').replace(' -','    '))
 
 
