3bd4291ca06017b5eaf83554b8f384c651af8361
altoidnerd <allenmajs1@gmail.com>
2017-07-10 00:28:50 -0400

    updates

--- src/efg.py
+++ src/efg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
-#!/usr/bin/env python3
-
+#
+#
+#
 #################################################################################
 #										#
 # Copyright (c) 2016 Allen Majewski (altoidnerd)				#
@@ -31,16 +31,35 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import matrix as mat
 
 
+def filtr(pattern, array):
+  """
+  filtr(str "parrtern", list array): -> list
+  
+    returns a new list containing the elements
+    in array for which "pattern" is matched.
+  """
+  return list( filter( lambda x: pattern in x, array ) )
 
-def filtrByComp(term, arr):
-  return [ line for line in arr if term in line ]
+def indices(pattern, array):
+  """
+  indices(str "pattern", array): -> list
+  
+    returns a new list containing the indices 
+    of the elements in array that match "pattern."
+  """
+  return [ array.index(thing) for thing in filtr(pattern, array) ]
 
-def filtrByLambda(term, arr):
-  return list(filter(lambda x: term in x, arr))
+def lmap(func, array):
+  """
+  lmap(function, array): -> list
 
-filtr=filtrByComp
+  just like python map, but returns a list instead of a
+ """
+  return list( map( func, array ) ) 
+
 
 
 class Efg(object):
@@ -48,24 +67,12 @@ class Efg(object):
   Class for parsing gipaw.x output files
   for 'efg' calculation
   """
-  # special paradichlorobenzene stuff
-
-  #           [ carbons             ,      hydrogens, chlorines ]
-  molecule1 = [2,  3,  5, 17, 16, 14,  4,  6, 15, 13,  1, 18]
-  molecule2 = [23, 8, 10, 11, 20, 22,  7,  9, 19, 21, 24, 12]
   
   def __init__(self, efgfile):
     self.efgfile = efgfile
     self.efgfile_array = open(efgfile, 'r').readlines()
     self.file_array = self.efgfile_array
-    
-    # ascertain nat
-    nat = len([ line for line in self.file_array if 'Q=' in line ])
-    self.nat = nat
-
-    self.WALL_TIME = float([ line.split()[4] for line in filtr('WALL',self.file_array) ][-1].replace('s',''))
-    self.CPU_TIME  = float([ line.split()[2] for line in filtr('WALL',self.file_array) ][-1].replace('s',''))
-
+  
 
   def __repr__(self):
     return "< efg object; efgfile: {}\tlength: {} lines >".format(self.efgfile, len(self.efgfile_array))
@@ -80,4 +87,98 @@ class Efg(object):
     for line in self.file_array:
       sys.stdout.write(line)
 
+
+  @property
+  def nat(self):
+    return len([ line for line in self.file_array if 'Q=' in line ])
+
     
+  @property
+  def WALL_TIME(self):
+    return float([ line.split()[4] for line in filtr('WALL',self.file_array) ][-1].replace('s',''))
+
+    
+  @property
+  def CPU_TIME(self):
+    return float([ line.split()[2] for line in filtr('WALL',self.file_array) ][-1].replace('s',''))
+
+
+  @property
+  def atom_labels(self):
+    a =  filtr("Q=", self.file_array)
+    b =  [ line.strip()[:6] for line in a ]
+    return b
+
+
+  @property
+  def atomic_species(self):
+    a =  filtr("Q=",self.file_array)
+    b =  [ line.split()[0] for line in a ]
+    return b
+
+
+  @property
+  def efg_tensors(self, atom=None):
+    pass
+
+
+  @property
+  def principle_axes(self):
+    axes=[]
+    for i in range(self.nat):
+      label = self.atom_labels[i]
+      vijs = filtr(label, self.file_array)[-4:][:3]
+      Xaxis  = lmap(float, filtr('Vxx', vijs)[0].replace(')','').replace('(','').split()[5:])
+      Yaxis  = lmap(float, filtr('Vyy', vijs)[0].replace(')','').replace('(','').split()[5:])
+      Zaxis  = lmap(float, filtr('Vzz', vijs)[0].replace(')','').replace('(','').split()[5:])
+      ax = []
+      ax.append(Xaxis)
+      ax.append(Yaxis)
+      ax.append(Zaxis)
+      axes.append(ax)
+    return axes
+
+
+
+  @property
+  def Vii(self):
+    viis = []
+    for i in range(self.nat):
+      label = self.atom_labels[i]
+      vijs = filtr(label, self.file_array)[-4:][:3]
+      vxx  = float(filtr('Vxx', vijs)[0].split()[3])
+      vyy  = float(filtr('Vyy', vijs)[0].split()[3])
+      vzz  = float(filtr('Vzz', vijs)[0].split()[3])
+      vii  = [vxx,vyy,vzz]
+      viis.append(vii)
+    return viis
+      
+      
+  @property
+  def Qs(self, indices=None):
+    a =  filtr("Q=",self.file_array)
+    b =  [ line.split()[3] for line in a ]
+    return lmap(float, b)
+    
+
+  @property
+  def Cqs(self, indices=None):
+    a =  filtr("Cq=",self.file_array)
+    b =  [ line.split()[7] for line in a ]
+    return lmap(float, b)
+
+
+  @property
+  def etas(self, indices=None):
+    a =  filtr("eta=",self.file_array)
+    b =  [ line.split()[10] for line in a ]
+    return lmap(float, b)
+
+
+  def compute_eta(self,atomic_specie_index):
+    ind = atomic_specie_index
+    return (self.Vii[ind][0] - self.Vii[ind][1])/self.Vii[ind][2]
+
+
+
+
