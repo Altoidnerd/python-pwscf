5e6c51ba6a38f2bc514ce36dc4e79779d6f62611
altoidnerd <allenmajs1@gmail.com>
2017-08-07 20:55:20 -0400

    updates

--- src/test/imports.py
+++ src/test/imports.py
@@ -5,15 +5,20 @@ import efg as efg
 import efgdev as efgdev
 from efg import *
 
-xmd=md.Md('md.in', 'md.out')
+#xmd=md.Md('md.in', 'md.out')
 
 #pos1=x.get_trajectory1()
 #pos2=x.get_trajectory2()
 #pos3=x.get_trajectory3()
 
 
-fstring='scfs/efg.{}.out'
-xefg=efg.Efg(fstring.format(0))
-xefgdev=efg.Efg(fstring.format(0))
+#fstring='scfs/efg.{}.out'
+#xefg=efg.Efg(fstring.format(0))
+#xefgdev=efg.Efg(fstring.format(0))
+
+
+import magres as mg
+fstring='scfs/scf-{}.efg.magres'
+xmg=mg.Magres(fstring.format(0))
 
 
