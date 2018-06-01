e77ba7f4bd4f9735d022a27f469609a4057e2bcf
Altoidnerd <Altoidnerd@users.noreply.github.com>
2018-05-31 21:13:05 -0400

    Update sequence_of_stddevs_of_partial_means.py

--- src/sequence_of_stddevs_of_partial_means.py
+++ src/sequence_of_stddevs_of_partial_means.py
@@ -3,7 +3,7 @@
 #
 #################################################################################
 #										#
-# Copyright (c) 2016 Allen Majewski (altoidnerd)				#
+# Copyright (c) 2018 Allen Majewski (altoidnerd)				#
 # Permission is hereby granted, free of charge, to any person obtaining a 	#
 # copy of this software and associated documentation files (the "Software"),	#
 # to deal in the Software without restriction, including without limitation	#
